eff23f2c7efa271fc75bb1fa874b318aa112e350
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-03-20 17:05:28 -0400

    seed data for companies and users

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,6 +3,14 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -26,14 +34,6 @@ const User = db.define('user', {
   },
   googleId: {
     type: Sequelize.STRING
-  },
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: Sequelize.STRING,
-    allowNull: false
   },
   isAdmin: {
     type: Sequelize.BOOLEAN,
